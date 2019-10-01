1f3964237ca6eb0e91afcd8c606e6d89d356836e
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-10-01 14:06:50 -0500

    Se agrega Gif y se cambia texto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tenor.gif';
 import './App.css';
 
 function App() {
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aquí estará mi portafolio
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
