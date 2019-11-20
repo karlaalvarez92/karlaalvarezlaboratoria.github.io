1982b16e9375562349fdc7b312772fbd580b1196
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-11-19 19:38:47 -0600

    Se agregan elementos al json, se corrige css

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -7,7 +7,7 @@ export default function Skills({ title, softSkills, technicallSkills, id }) {
     <div className={"skills-section"} id={id}>
        <div className="skills-tec" id={id}>
        <h1 className = "tec-title">Tools</h1>
-        <ul className = "technicallList">
+        <ul className = "tecList">
           {technicallSkills && technicallSkills.map(el => 
             <li className= "list-tec" key={el}>{el}</li>)}
         </ul>
