0197ce7212c4ee12de4533c6968002f370ad13d4
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-11-26 18:32:32 -0600

    Componente AboutMe responsive y con transparencias

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,11 +4,11 @@ import './AboutMe.css'
 export default function About({ title, id, aboutMe }) {
   
   return (
-    <div className="aboutMeBackground">
-      <div className="about-content" id={id}>
+    <main>
+      <div id={id}>
         <h1 className= "about-title">{title}</h1>
         {aboutMe && aboutMe.map((element, index) => <p key={index}>{element}</p>)}   
       </div>
-    </div>
+    </main>
   );
 }
