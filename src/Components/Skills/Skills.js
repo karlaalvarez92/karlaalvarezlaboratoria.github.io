fa30061c6f8935f6e05aa7f9cc95110853a9b67a
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-11-28 18:42:08 -0600

    Se cambia orden de Tools y Soft,se cambian colores, se hace responsive ambos elementos, se deja espacio para iconos de tecnologías

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import './Skills.css'
+import './Skills.css';
+import onda from "./onda-correcta.png";
 
-export default function Skills({ title, softSkills, technicallSkills, id }) {
+export default function Skills({ techIcons, softSkills, technicallSkills, id }) {
   
   return (
     <div className="skills-container" id={id}>
-       <section className="tech" id={id}>
-       <h1 className = "tools">Tools</h1>
-        <ul>
-          {technicallSkills && technicallSkills.map(el => 
-            <li key={el}>{el}</li>)}
-        </ul>
-      </section>
-
-      {/* <img src="https://i.ibb.co/31WggTW/onda-correcta.png" alt=""/> */}
-
       <section className= "soft">
         <h1 className = "softSkills">Soft skills</h1> 
         <ul>
@@ -22,6 +13,21 @@ export default function Skills({ title, softSkills, technicallSkills, id }) {
             <li key={el}>{el}</li>)}
         </ul>
       </section>
+
+      <img src= {onda} /*"https://i.ibb.co/31WggTW/onda-correcta.png"*/ alt=""/>
+
+      <section className="tech" id={id}>
+       <h1 className = "tools">Tools</h1>
+        <div>
+          {techIcons && techIcons.map(el =>
+            <img alt=" " src={el}></img>)}
+        </div>
+        <ul>
+          {technicallSkills && technicallSkills.map(el => 
+            <li key={el}>{el}</li>)}
+        </ul>
+      </section>
+      
      </div>
   );
 }
