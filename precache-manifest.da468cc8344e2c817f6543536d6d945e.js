b2ddce4cff61731d2b8d00ac2f880f683d79a01c
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-11-29 21:12:54 -0600

    Corrección de carpetas

---
+++ precache-manifest.da468cc8344e2c817f6543536d6d945e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c64f72cecd52a949f1411c64bd29726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cff31dd55565c36a087",
+    "url": "/static/css/main.3f080c9f.chunk.css"
+  },
+  {
+    "revision": "d01c42777e412801519a",
+    "url": "/static/js/2.202c3a60.chunk.js"
+  },
+  {
+    "revision": "5cff31dd55565c36a087",
+    "url": "/static/js/main.2e99c845.chunk.js"
+  },
+  {
+    "revision": "cd2c69c3a11faa5173ed",
+    "url": "/static/js/runtime-main.ea3c4911.js"
+  },
+  {
+    "revision": "811e7f69d896b5ab8e62b3f8f0464460",
+    "url": "/static/media/fondo-natural-paisaje-acuarela.811e7f69.png"
+  }
+]);
